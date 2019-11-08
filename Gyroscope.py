e2356a33ad2e294bb77a65ee5515cd773346b27d
Javier Macías <jamaciastf@gmail.com>
2019-11-08 17:40:15 +0200

    Added the computation of the covariance matrix of the gyro

--- Gyroscope.py
+++ Gyroscope.py
@@ -8,6 +8,7 @@ class Gyroscope(object):
         self.file = file
         self.bias = [] # x y z bias 
         self.gyro_xyz = []
+        self.covariance_matrix = []
     
 
     def read_data(self, file):
@@ -18,10 +19,16 @@ class Gyroscope(object):
     def calibrate(self):
         self.read_data(self.file)
         self.compute_bias()
+        self.compute_covariance()
     
     def compute_bias(self):
         gyro_bias_x = np.mean(self.gyro_xyz.iloc[:,0])
         gyro_bias_y = np.mean(self.gyro_xyz.iloc[:,1])
         gyro_bias_z = np.mean(self.gyro_xyz.iloc[:,2])
         self.bias=  [gyro_bias_x, gyro_bias_y,gyro_bias_z]
-        print(self.bias)
+        print("Gyroscope bias: ", self.bias)
+
+    def compute_covariance(self):
+        # self.gyro_xyz has the variables as columns. np.conv uses rows for vars by default.
+        self.covariance_matrix = np.cov(self.gyro_xyz, rowvar=False)
+        print("Covariance matrix: ", self.covariance_matrix)
