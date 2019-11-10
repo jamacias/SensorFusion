localization work but there is a fix to do with some sign in the wall

Camera.py
@@ -21,20 +21,33 @@ class Camera(object):
         
     
     def calibrate(self):
-        dist_cam_qr_cm, height_qr , x_pos= self.read_data()
+        dist_cam_qr_cm, height_qr , x_pos = self.read_data()
+        #data collected manually
+        height_qr = np.array([36.8,41.4,46.4,51.4,56.4,61.4,66.4,71.4,76.4,81.4,86.4,91.4,96.4,101.4,106.4,111.4,116.4,121.4,126.4,131.4,136.4])
+        dist_cam_qr_cm = [176,156,137,124,112,103,96,88,83,78,73,68,65,62,60,55,53,52,50,47,46]
+
+
         # least square error for fitting line
         A = np.vstack([1/height_qr, np.ones(len(1/height_qr))]).T
-        dist_cam_qr = np.array(dist_cam_qr_cm.values)
+        dist_cam_qr = np.array(dist_cam_qr_cm)
         m, b = np.linalg.lstsq(A, dist_cam_qr, rcond=None) [0]
         self.focal_lenght = m/self.qr_dimension
         self.bias = b
 
         print("f_l : ", self.focal_lenght)
+        print("bias : ", self.bias)
 
-        print(np.arctan2(x_pos,self.focal_lenght)*180/np.pi)
-        
         #plt.plot(1/height_qr, m*(1/height_qr) + b, 'r', label='Fitted line')
         #plt.legend()    
         #plt.plot(1/height_qr, dist_cam_qr_cm,'.')
-        #plt.plot(camera_data.iloc[:, 3])
         #plt.show()
+
+    def get_angle(self, qr_x, qr_num):
+        return [(np.arctan2(qr_x,self.focal_lenght)*180/np.pi) , qr_num]
+
+    def get_distance(self,qr_h):
+        return (self.qr_dimension*self.focal_lenght)/qr_h + self.bias
+    
+    def localize(self):
+        camera_localization = pd.read_csv("./data/CameraModuleLocalization1.csv")
+        print(camera_localization)